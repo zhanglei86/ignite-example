10454bf4f5374bf0e9d8624bd97ba66d22c16dfd
zhanglei <zhanglei863@crnet.com.hk>
2018-12-27 13:39:59 +0800

    #2.add

---
+++ src/main/java/win/leizhang/demo/igniteexample/IgniteExampleApplication.java
@@ -0,0 +1,14 @@
+package win.leizhang.demo.igniteexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteExampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/win/leizhang/demo/igniteexample/IgniteExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package win.leizhang.demo.igniteexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IgniteExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
